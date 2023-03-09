e157e1306084452c3ca7a8d41a6eb908bd496bb4
Netzer-yech <netzer.yech@gmail.com>
2023-03-09 11:03:30 +0200

    editing the rates file

--- rates.py
+++ rates.py
@@ -1,11 +1,17 @@
-import requests
-import json
 
-api_key = "caad6a6a74764f9caa9628e1c8c2fa4a"
-base_url = "https://docs.openexchangerates.org/reference/latest-json"
-# params = {"net_1": api_key}
-#
-# response = requests.get(base_url, params=params)
-# if response.status_code = 200 :
+# import requests
+
+# base_currency = "USD"
+# target_currency = "ILS"
+# api_key = "caad6a6a74764f9caa9628e1c8c2fa4a"
+
+# api_url = f"https://openexchangerates.org/api/latest.json?app_id={api_key}&base={base_currency}&symbols={target_currency}"
+# response = requests.get(api_url)
+
+# if response.status_code == 200:
 #     data = response.json()
-#     usd_rate = data ['rates'] ['']
+#     rate = data["rates"][target_currency]
+#     print(f"1 {base_currency} = {rate} {target_currency}")
+# else:
+#     print("Could not get rate from API using default rate...")
+#     rate = 0.28
